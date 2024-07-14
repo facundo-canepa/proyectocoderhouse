18dad417ceb196877538d977ed9fdb8119224a3b
Facundo Cánepa <facundo.villalba.canepa@gmail.com>
2024-07-14 18:10:39 -0300

    Aegrgo mensaje 3

--- ejemplo1.py
+++ ejemplo1.py
@@ -11,3 +11,6 @@ def multiplicar(n1,n2):
     print("el resultado de la multiplicación entre n1 * n2: ", n1 * n2)
     
     multiplicar(int(input("Ingresa el primer número: ")),int(input("Ingresa el segundo número: ")))
+
+def mensaje3():
+    print("Estoy en la rama facundo.canepa")
